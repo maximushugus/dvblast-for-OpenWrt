f97a959488bee937ac0b2970109c1edf56e89cf4
maximushugus <36080343+maximushugus@users.noreply.github.com>
2020-03-17 19:05:35 +0100

    Add files via upload

---
+++ sources/bitstream/mpeg/psi/descs_list.h
@@ -0,0 +1,72 @@
+/*****************************************************************************
+ * descs_list.h: All supported ISO/IEC 13818-1 descriptors
+ *****************************************************************************
+ * Copyright (C) 2009-2010 VideoLAN
+ *
+ * Authors: Christophe Massiot <massiot@via.ecp.fr>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+
+#ifndef __BITSTREAM_MPEG_DESCS_LIST_H__
+#define __BITSTREAM_MPEG_DESCS_LIST_H__
+
+/*
+ * 1. Keep the list ordered.
+ * 2. When you are adding new descriptor here, make sure you also add
+ *    the needed code in bitstream/mpeg/psi/descs_print.h
+ */
+
+#include "desc_02.h"
+#include "desc_03.h"
+#include "desc_04.h"
+#include "desc_05.h"
+#include "desc_06.h"
+#include "desc_07.h"
+#include "desc_08.h"
+#include "desc_09.h"
+#include "desc_0a.h"
+#include "desc_0b.h"
+#include "desc_0c.h"
+#include "desc_0d.h"
+#include "desc_0e.h"
+#include "desc_0f.h"
+#include "desc_10.h"
+#include "desc_11.h"
+#include "desc_12.h"
+#include "desc_1b.h"
+#include "desc_1c.h"
+#include "desc_1d.h"
+#include "desc_1e.h"
+#include "desc_1f.h"
+#include "desc_20.h"
+#include "desc_21.h"
+#include "desc_22.h"
+#include "desc_23.h"
+#include "desc_24.h"
+#include "desc_25.h"
+#include "desc_26.h"
+#include "desc_27.h"
+#include "desc_28.h"
+#include "desc_2a.h"
+#include "desc_2b.h"
+#include "desc_2c.h"
+
+#endif
