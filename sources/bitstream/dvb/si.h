4b758cf11386d65b863c3814596b989766ffc6b2
maximushugus <36080343+maximushugus@users.noreply.github.com>
2020-03-17 19:01:46 +0100

    Add files via upload

---
+++ sources/bitstream/dvb/si.h
@@ -0,0 +1,55 @@
+/*****************************************************************************
+ * si.h: ETSI EN 300 468 Service Information
+ *****************************************************************************
+ * Copyright (C) 2009-2010 VideoLAN
+ *
+ * Authors: Christophe Massiot <massiot@via.ecp.fr>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject
+ * to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+ * CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+ * SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ *****************************************************************************/
+
+/*
+ * Normative references:
+ *  - ISO/IEC 13818-1:2007(E) (MPEG-2 Systems)
+ *  - ETSI EN 300 468 V1.11.1 (2010-04) (SI in DVB systems)
+ */
+
+#ifndef __BITSTREAM_DVB_SI_H__
+#define __BITSTREAM_DVB_SI_H__
+
+#include "../common.h"
+#include "../mpeg/psi/psi.h"
+#include "../mpeg/psi/descriptors.h"
+#include "si/numbers.h"
+#include "si/datetime.h"
+#include "si/strings.h"
+#include "si/descs_list.h"
+#include "si/nit.h"
+#include "si/bat.h"
+#include "si/sdt.h"
+#include "si/eit.h"
+#include "si/tdt.h"
+#include "si/tot.h"
+#include "si/rst.h"
+#include "si/st.h"
+#include "si/dit.h"
+#include "si/sit.h"
+
+#endif
